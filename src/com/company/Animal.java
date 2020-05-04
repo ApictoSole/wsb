64b82d314565ac872807dbb17d44125f2947ec95
ApictoSole <mrapictosole@gmail.com>
2020-05-04 16:58:51 +0200

    Task 2: Pet process

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,44 @@
 package com.company;
 
 public class Animal {
+    final String species;
+    private Double weight;
+
+    public Animal(String species) {
+        this.species = species;
+        switch (species) {
+            case "dog":
+                this.weight = 15.0;
+                break;
+            case "cat":
+                this.weight = 10.0;
+                break;
+            case "myszojeleń":
+                this.weight = 5.5;
+                break;
+            default:
+                this.weight = 1.0;
+                break;
+        }
+    }
+
+    private boolean isAlive() {
+        return this.weight > 0;
+    }
+
+    public void feed() {
+        if (isAlive()) {
+            this.weight++;
+            System.out.println("Fed. Weight increased to " + this.weight + " kg");
+        }
+        else System.out.println("Call the shaman !!!");
+    }
+
+    public void takeForAWalk() {
+        if (isAlive()) {
+            this.weight--;
+            System.out.println("Fitness" + this.weight + " kg");
+        }
+        else System.out.println("I'm not zombie i cant walk when i am dead");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Human me = new Human();
 
+        me.firstName = "Bartosz";
+        me.lastName = "Mazur";
+        me.pet = new Animal("dog");
+
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.takeForAWalk();
     }
 }
